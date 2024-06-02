06878c9b40c10a5775fa6ba61ed348b325425f90
Maksym Yarema <macsym2@gmail.com>
2024-06-02 15:04:19 +0300

    init project

---
+++ src/test/java/org/yaremax/liquibasepractice/LiquibasePracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yaremax.liquibasepractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquibasePracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
